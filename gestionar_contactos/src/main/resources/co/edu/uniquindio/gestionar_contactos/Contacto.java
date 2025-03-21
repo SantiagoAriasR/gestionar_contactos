6ed42d94c6ea5761c8ff76df83fae41b1582ce0c
SantiagoAriasR <santiago.ariasr1@uqvirtual.edu.co>
2025-03-20 19:30:30 -0500

    Add files via upload
    Clases

---
+++ gestionar_contactos/src/main/resources/co/edu/uniquindio/gestionar_contactos/Contacto.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.gestionar_contactos.Models;
+
+import javafx.scene.image.Image;
+import lombok.*;
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class Contacto {
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String correo;
+    private LocalDate diaCumpleaños;
+    private Image fotoPerfil;
+}
+2
+
+
