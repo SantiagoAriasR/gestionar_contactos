04c24652be71385b4aea9ff1e6777c151f69a329
santiagooff <santiago.olarteb@uqvirtual.edu.co>
2025-03-27 22:06:16 -0500

    Manejar excepciones para espacios incompletos o mal rellenos (gestion contactos)

--- gestionar_contactos/src/main/java/co/edu/uniquindio/gestionar_contactos/Models/GestionContactos.java
+++ gestionar_contactos/src/main/java/co/edu/uniquindio/gestionar_contactos/Models/GestionContactos.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.gestionar_contactos.Models;
 
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import lombok.*;
 
@@ -23,23 +24,33 @@ public class GestionContactos {
     public void registrarContacto(Contacto contacto) throws Exception{
 
         if(contacto.getNombre() == null || contacto.getNombre().isEmpty()){
-            throw new Exception("El id es obligatorio");
+            Exception nombreVacio = new Exception("El nombre es obligatorio");
+            manejarExcepciones(nombreVacio);
+            throw nombreVacio;
         }
 
         if(contacto.getApellido() == null || contacto.getApellido().isEmpty()){
-            throw new Exception("El nombre es obligatorio");
+            Exception apellidoVacio = new Exception("El apellido es obligatorio");
+            manejarExcepciones(apellidoVacio);
+            throw apellidoVacio;
         }
 
-        if(contacto.getTelefono() == null || contacto.getTelefono().isEmpty()){
-            throw new Exception("La dirección es obligatoria");
+        if(contacto.getTelefono() == null || contacto.getTelefono().isEmpty() || contacto.getTelefono().length() < 10){
+            Exception telefonoVacio = new Exception("El telefono es obligatorio");
+            manejarExcepciones(telefonoVacio);
+            throw telefonoVacio;
         }
 
         if(contacto.getCorreo() == null || contacto.getCorreo().isEmpty()){
-            throw new Exception("El email es obligatorio");
+            Exception correoVacio = new Exception("El email es obligatorio");
+            manejarExcepciones(correoVacio);
+            throw correoVacio;
         }
 
         if(contacto.getDiaCumpleanos() == null){
-            throw new Exception("La contraseña es obligatoria");
+            Exception cumpleanosVacio = new Exception("La cumpleaños es obligatorio");
+            manejarExcepciones(cumpleanosVacio);
+            throw cumpleanosVacio;
         }
 
         if(buscarContacto(contacto.getTelefono()) != null){
@@ -85,5 +96,13 @@ public class GestionContactos {
             }
         }
         return centinela;
+    }
+
+    public void manejarExcepciones(Exception e){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("No puedes dejar espacios vacios");
+        alert.setContentText(e.getMessage());
+        alert.showAndWait();
     }
 }
